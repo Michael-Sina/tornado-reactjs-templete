1b8c4bb855f2df72752c2c70ed88b2a6a027b0a2
Mike <chutian.8986@gmail.com>
2018-05-25 16:09:20 +0900

    :art: impoved homepage

--- react-client/src/routes/Home.js
+++ react-client/src/routes/Home.js
@@ -1,48 +1,65 @@
 import React from 'react';
 import { Link } from 'dva/router';
-import { Menu, Icon } from 'antd';
+import { Layout, Menu, Icon } from 'antd';
 import style from './Home.less';
 
+const { Header, Sider, Content } = Layout;
 const SubMenu = Menu.SubMenu;
-const MenuItem = Menu.Item;
 
 class Home extends React.Component {
+  state = {
+    collapsed: false,
+  };
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  }
   render () {
-    // const {children} = this.props;
     return (
-      <div>
-        <header className={style.header}>
-          <Link to="/">ダッシュボード</Link>
-        </header>
-
-        <main className={style.main}>
-          <div className={style.menu}>
-            <Menu mode="inline" theme="dark" style={{width: '240px'}}>
-              <SubMenu key="user" title={<span><Icon type="user"/><span>アカウント管理</span></span>}>
-                <MenuItem key="user-list">
-                  <Link to="/user/list">アカウント一覧</Link>
-                </MenuItem>
-                <MenuItem key="user-add">
-                  <Link to="/user/add">アカウント追加</Link>
-                </MenuItem>
-              </SubMenu>
-
-              <SubMenu key="book" title={<span><Icon type="book"/><span>書籍管理</span></span>}>
-                <MenuItem key="book-list">
-                  <Link to="/book/list">書籍管理</Link>
-                </MenuItem>
-                <MenuItem key="book-add">
-                  <Link to="/book/add">書籍追加</Link>
-                </MenuItem>
-              </SubMenu>
-            </Menu>
-          </div>
-
-          <div className={style.content}>
-            Welcome
-          </div>
-        </main>
-      </div>
+      <Layout style={{ minHeight: '100vh' }}>
+        <Sider
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className={style.logo} />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <SubMenu
+              key="user"
+              title={<span><Icon type="user" /><span>アカウント管理</span></span>}
+            >
+              <Menu.Item key="user-list">
+                <Link to="/user/list">アカウント一覧</Link>
+              </Menu.Item>
+              <Menu.Item key="user-add">
+                <Link to="/user/add">アカウント追加</Link>
+              </Menu.Item>
+            </SubMenu>
+            <SubMenu
+              key="book"
+              title={<span><Icon type="book" /><span>書籍管理</span></span>}
+            >
+              <Menu.Item key="book-list">
+                <Link to="/book/list">書籍管理</Link>
+              </Menu.Item>
+              <Menu.Item key="book-add">
+                <Link to="/book/list">書籍追加</Link>
+              </Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding: 0 }}>
+            <Icon
+              className={style.trigger}
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+          </Header>
+          <Content className={style.content} />
+        </Layout>
+      </Layout>
     );
   }
 }
