dc6291a4c0a313627b9112e45d61bd8ea79f1bbe
Mike <chutian.8986@gmail.com>
2018-06-04 18:21:09 +0900

    :zap: update BookEditor

---
+++ react-client/src/components/Books/BookEditorForm.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import { Form, Input, Button, Select } from 'antd';
+
+const FormItem = Form.Item;
+const SelectOption = Select.Option;
+const { TextArea } = Input;
+
+const BookEditorForm = ({form}) => {
+    const { getFieldDecorator } = form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 6 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 12 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
+    return (
+        <Form onSubmit={this.handleSubmit}  style={{width: '100%', padding: '80px'}}>
+          <FormItem
+            {...formItemLayout}
+            label={(
+              <span>タイトル</span>
+            )}
+          >
+            {getFieldDecorator('title', {
+              rules: [{ required: true, message: 'タイトルを入力してください!', whitespace: true }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={(
+              <span>出版社</span>
+            )}
+          >
+            {getFieldDecorator('publisher', {
+              rules: [{ required: true, message: '出版社を入力してください!', whitespace: true }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={(
+              <span>作者</span>
+            )}
+          >
+            {getFieldDecorator('author', {
+              rules: [{ required: true, message: '作者を入力してください!', whitespace: true }],
+            })(
+              <Input />
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label="フォーマット"
+          >
+            {getFieldDecorator('format', {
+              rules: [{
+                required: true, message: 'フォーマットを選択してください!',
+              }],
+            })(
+              <Select
+                placeholder="フォーマットを選択する"
+                onChange={this.handleProvinceChange}>
+                <SelectOption value="紙">紙</SelectOption>
+                <SelectOption value="電子書籍">電子書籍</SelectOption>
+              </Select>
+            )}
+          </FormItem>
+          <FormItem
+            {...formItemLayout}
+            label={(
+              <span>感想</span>
+            )}
+          >
+            {getFieldDecorator('thought', {
+              rules: [{ required: true, message: '感想を入力してください!', whitespace: true }],
+            })(
+              <TextArea rows={15} />
+            )}
+          </FormItem>
+          <FormItem {...tailFormItemLayout}>
+            <Button type="primary" htmlType="submit">保存</Button>
+          </FormItem>
+        </Form>
+      );
+}
+
+export default Form.create()(BookEditorForm);
